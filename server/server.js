fb5784a2e4ab529a82ff268ddc925284d11ab9b8
Ashish <ashishojha19980@gmail.com>
2026-04-03 02:37:45 +0530

    fix cors for socket for deploy

--- server/server.js
+++ server/server.js
@@ -21,7 +21,9 @@ const server = http.createServer(app);
 // ===============================
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // frontend URL
+    origin:["http://localhost:5173",
+       "https://your-app.vercel.app"
+    ], // frontend URL
    
     credentials: true
   
@@ -79,7 +81,10 @@ server.on("error", (err) => {
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin:[ "http://localhost:5173",
+     "https://your-app.vercel.app"
+  ],
+
   credentials: true
 }));
 app.use('/api/status',(req,res)=>{
